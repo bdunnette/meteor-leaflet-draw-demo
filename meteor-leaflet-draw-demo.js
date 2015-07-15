7398b227a85b8239a2290fb41cc852072568d035
Brian Dunnette <brian@dunnette.us>
2015-07-15 14:44:09 -0500

    removing method

--- meteor-leaflet-draw-demo.js
+++ meteor-leaflet-draw-demo.js
@@ -98,14 +98,7 @@ if (Meteor.isClient) {
 if (Meteor.isServer) {
   Meteor.startup(function() {
     // code to run on server at startup
-    Queue.setInterval('deleteAllMarkers','Meteor.call("deleteAllMarkers")', 86400000); /* once a day */
+    Queue.setInterval('deleteAllMarkers','Markers.remove({})', 86400000); /* once a day */
     Queue.run();
-  });
-  
-  Meteor.methods({
-    'deleteAllMarkers': function(){
-      console.log("Removing markers...");
-      Markers.remove({});
-    }
   });
 }
